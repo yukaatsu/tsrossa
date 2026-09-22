4d59192a0b725ea9332318add21b4e93ba6d90de
Yuka <yukaatsu2773@gmail.com>
2026-09-23 02:00:57 +0700

    chore(release): bump version to v1.2.0-beta3 and build release APK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.yuka.musicplayer"
         minSdk = 26
         targetSdk = 34
-        versionCode = 3
-        versionName = "1.2.0-beta2"
+        versionCode = 4
+        versionName = "1.2.0-beta3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
